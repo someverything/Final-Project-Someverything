2a069e35c806024cafbe5e2f1ccc1b0d1a8db710
someverything <rustameyvaz@gmail.com>
2024-08-17 17:03:02 +0400

    IAuthService-Confirmed

--- Business/Abstract/IAuthService.cs
+++ Business/Abstract/IAuthService.cs
@@ -1,4 +1,6 @@
-﻿using Core.Utilities.Results.Abstract;
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results.Abstract;
+using Entities.DTOs.AuthDTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,11 @@ namespace Business.Abstract
 {
     interface IAuthService
     {
-        Task<IResult> RegisterAsync(RegisterDTO)
+        Task<IResult> RegisterAsync(RegisterDTO model);
+        Task<IDataResult<Token>> LoginAsync(LoginDTO loginDTO);
+        Task<IDataResult<string>> UpdateRefreshTokenAsync(string refreshToken, AppUser appUser);
+        Task<IDataResult<Token>> RefreshTokenLoginAsync(string refreshToken);
+        Task<IResult> AssignRoleToUserAsync(string userId, string roleName);
+        Task<IResult> UserEmailConfirmed(string email, string otp);
     }
 }
